48577462a2960a5d538afe10b823db4164e14853
Rowan Tran <rowanjames.tran@gmail.com>
2016-06-26 10:09:10 -0700

    Add settings activity (not respected)

---
+++ app/src/main/java/com/codepath/taskhelper/SettingsFragment.java
@@ -0,0 +1,13 @@
+package com.codepath.taskhelper;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
