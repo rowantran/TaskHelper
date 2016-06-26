997bd75d6f8394aaa6f9262b72326b4c4edb51e0
Rowan Tran <rowanjames.tran@gmail.com>
2016-06-25 21:12:35 -0700

    TasksAdapter shows priority

--- app/src/main/java/com/codepath/todoapp/TasksAdapter.java
+++ app/src/main/java/com/codepath/todoapp/TasksAdapter.java
@@ -1,17 +1,22 @@
 package com.codepath.todoapp;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
 public class TasksAdapter extends ArrayAdapter<Task> {
+    Context context;
+
     public TasksAdapter(Context context, List<Task> tasks) {
         super(context, 0, tasks);
+        this.context = context;
     }
 
     @Override
@@ -21,8 +26,19 @@ public class TasksAdapter extends ArrayAdapter<Task> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_task, parent, false);
         }
 
-        TextView tvBody = (TextView) convertView.findViewById(R.id.tvBody);
+        TextView tvBody = (TextView) convertView.findViewById(R.id.tv_body);
         tvBody.setText(task.body);
+
+        ImageView ivPriority = (ImageView) convertView.findViewById(R.id.iv_priority);
+        int drawableID;
+        if (task.priority == 0) {
+            drawableID = R.drawable.priority_low;
+        } else if (task.priority == 1) {
+            drawableID = R.drawable.priority_medium;
+        } else {
+            drawableID = R.drawable.priority_high;
+        }
+        ivPriority.setImageDrawable(ContextCompat.getDrawable(context, drawableID));
 
         return convertView;
     }
